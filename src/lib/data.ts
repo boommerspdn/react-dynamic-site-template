f4d2aae81bba79dead7d333e9c7cbfd16981ffa1
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2024-01-13 16:35:32 +0700

    return the whole useQuery

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -31,36 +31,25 @@ export const useReactQueryGet = <T>(
   query: object | null | undefined,
   key: Array<string>,
 ) => {
-  const { data, isLoading, error } = useQuery<T>({
+  return useQuery<T>({
     queryFn: () =>
       axios
         .get(`${import.meta.env.VITE_API_URL}${url}`, { params: query })
         .then((res) => res.data),
     queryKey: key,
   });
-
-  return {
-    data,
-    isLoading,
-    error,
-  };
 };
 
 export const useReactQueryPost = <T>(url: string, key: Array<string>) => {
   const queryClient = useQueryClient();
 
-  const { mutate, isLoading } = useMutation({
+  return useMutation({
     mutationFn: (dataToPost: T) =>
       axios.post(`${import.meta.env.VITE_API_URL}${url}`, dataToPost),
     onSettled: async () => {
       return await queryClient.invalidateQueries({ queryKey: key });
     },
   });
-
-  return {
-    mutate,
-    isLoading,
-  };
 };
 
 export const useReactQueryPut = <T>(
@@ -70,18 +59,13 @@ export const useReactQueryPut = <T>(
 ) => {
   const queryClient = useQueryClient();
 
-  const { mutate, isLoading } = useMutation({
+  return useMutation({
     mutationFn: (dataToPost: T) =>
       axios.put(`${import.meta.env.VITE_API_URL}${url}/${id}`, dataToPost),
     onSettled: async () => {
       return await queryClient.invalidateQueries({ queryKey: key });
     },
   });
-
-  return {
-    mutate,
-    isLoading,
-  };
 };
 
 export const useReactQueryDelete = (
@@ -91,16 +75,11 @@ export const useReactQueryDelete = (
 ) => {
   const queryClient = useQueryClient();
 
-  const { mutate, isLoading } = useMutation({
+  return useMutation({
     mutationFn: () =>
       axios.delete(`${import.meta.env.VITE_API_URL}${url}/${id}`),
     onSettled: async () => {
       return await queryClient.invalidateQueries({ queryKey: key });
     },
   });
-
-  return {
-    mutate,
-    isLoading,
-  };
 };
